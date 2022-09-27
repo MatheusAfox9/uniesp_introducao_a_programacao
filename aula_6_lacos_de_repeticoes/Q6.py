c9c112e0f3b17621653e2a82790acdf31b03c6c8
Matheus Ataide <mateeeus.a@gmail.com>
2022-09-27 19:59:30 -0300

    Questões

--- aula_6_lacos_de_repeticoes/Q6.py
+++ aula_6_lacos_de_repeticoes/Q6.py
@@ -13,7 +13,24 @@ d. Modifique sua lista, substitua os desistentes por novos convidados.
 
 e. Exiba um novo convite para cada pessoa que continua presente em sua lista.'''
 
-lista_amigos = ["Neymar", "Vini Jr", "Bruxo", "Fenomeno", "Messi"]
+convidados = ["Neymar", "Vini Jr", "Bruxo", "Fenomeno", "Messi"]
 
-for nome in lista_amigos:
-    print(f"Olá {nome}, você foi convidade para minha revoada.")
+for nome in convidados:
+    mensagem = f"Bora pra balada, {nome}!"
+    print(mensagem)
+    
+
+#Quem não poderá ir
+print("Bruxo: Infelizmente não posso estar no mesmo ambiente que o Fenomeno")
+print("Fenomeno: Infelizmente não posso estar no mesmo ambiente que o Bruxo")
+
+#Novos convidados
+
+convidados[2] = "Matheus"
+convidados[0] = "Michel"
+
+for nome in convidados:
+    mensagem = f"Bora pra balada, {nome}!"
+    print(mensagem)
+    
+   
